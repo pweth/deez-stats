84a189397b3472ade15856d85947c0b44fda0699
Peter Wetherall <peter@wetherall.pw>
2020-06-22 19:19:39 +0100

    Logic files

---
+++ index.php
@@ -0,0 +1,45 @@
+<!doctype html>
+<html lang="en">
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Deez Stats</title>
+	<meta name="robots" content="index, follow">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="Analyze your Deezer listening history to discover your Deez rank, top tunes and favourite artists.">
+	<meta name="twitter:card" content="summary">
+	<meta name="twitter:url" content="https://deezstats.com">
+	<meta name="twitter:image" content="https://deezstats.com/favicon.png">
+	<meta property="og:type" content="website">
+	<meta property="og:url" content="https://deezstats.com">
+	<meta property="og:title" content="Deez Stats">
+	<meta property="og:description" content="Analyze your Deezer listening history to discover your Deez rank, top tunes and favourite artists.">
+	<meta property="og:image" content="https://deezstats.com/favicon.png">
+	<meta name="theme-color" content="#479ECC">
+	<link rel="icon" href="favicon.png">
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+	<content>
+		<div>
+			<h1 class="title"><img draggable="false" src="favicon.png"> Deez&nbsp;Stats</h1>
+			<h3 class="subtitle">
+				Analyze your Deezer <span class="history">listening history</span> to discover your <span class="genres">Deez rank</span>, <span class="tunes">top tunes</span> and <span class="artists">favourite artists</span>.
+			</h3>
+			<a class="auth" href="/auth">
+				<img draggable="false" src="img/deezer.png"><br><div>Login with Deezer</div>
+			</a>
+		</div>
+	</content>
+	<footer>
+		<div class="container">
+			<div class="left">
+				<a href="/terms">Terms Of Use</a>
+			</div>
+			<div class="right">
+				Built by <a href="https://github.com/peterwetherall/deez-stats" target="_blank">Peter</a> &#x1F3B5;
+			</div>
+		</div>
+	</footer>
+</body>
+</html>
---
+++ terms.php
@@ -0,0 +1,39 @@
+<!doctype html>
+<html lang="en">
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Deez Stats &#124; Terms Of Use</title>
+	<meta name="robots" content="noindex, nofollow">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="icon" href="favicon.png">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+	<style>
+		.container { text-align: justify; }
+		h4 { text-align: center; }
+		h6 { text-align: left; }
+	</style>
+</head>
+<body>
+	<div class="container">
+		<h4>Deez Stats Terms Of Use</h4>
+		<br>
+		<h6>This site is in no way sponsored, endorsed or administered by Deezer SA.</h6>
+		<h6>Deez Stats makes use of the Deezer Developer API (https://developers.deezer.com/) and was created as a personal, non-commercial project.</h6>
+		<br>
+		<p>For purposes of this Agreement, “Site” refers to this website, which can be accessed at https://deezstats.com.</p>
+		<p>“You” refers to you, as a user of our Site.</p>
+		<p>By accessing our Site, you accept our Terms Of Use.</p>
+		<br>
+		<h5>I.&nbsp;Cookies and Web Beacons</h5>
+		<p> Like any other website, Deez Stats uses ‘cookies’. These cookies are used to store information including visitors’ preferences, and the pages on the website that the visitor accessed or visited. The information is used to optimize the users’ experience by customizing our web page content based on visitors’ browser type and/or other information.</p>
+		<br><h5>II.&nbsp;Disclaimers</h5>
+		<p>The site is provided on an "as-is" and "as available" basis, and company and our suppliers expressly disclaim any and all warranties and conditions of any kind, whether express, implied, or statutory, including all warranties or conditions of merchantability, fitness for a particular purpose, title, quiet enjoyment, accuracy, or non-infringement.  We and our suppliers make not guarantee that the site will meet your requirements, will be available on an uninterrupted, timely, secure, or error-free basis, or will be accurate, reliable, free of viruses or other harmful code, complete, legal, or safe.  If applicable law requires any warranties with respect to the site, all such warranties are limited in duration to ninety (90) days from the date of first use.<br><br>Some jurisdictions do not allow the exclusion of implied warranties, so the above exclusion may not apply to you.  Some jurisdictions do not allow limitations on how long an implied warranty lasts, so the above limitation may not apply to you.</p>
+		<br><h5>III.&nbsp;Term and Termination</h5>
+		<p>Subject to this Section, these Terms will remain in full force and effect while you use the Site.  We may suspend or terminate your rights to use the Site at any time for any reason at our sole discretion, including for any use of the Site in violation of these Terms.  Upon termination of your rights under these Terms, your Account and right to access and use the Site will terminate immediately.  You understand that any termination of your Account may involve deletion of your User Content associated with your Account from our live databases.  Company will not have any liability whatsoever to you for any termination of your rights under these Terms.</p>
+		<br><h5>IV.&nbsp;Copyright Information</h5>
+		<p>All trademarks, logos and service marks displayed on the Site are our property or the property of other third-parties. You are not permitted to use these Marks without our prior written consent or the consent of such third party which may own the Marks.</p>
+		<br><h5>V.&nbsp;Contact Us</h5>
+		<p>If you have any questions regarding this Terms Of Use or the practices of this Site, please contact us by sending an email to <a href="mailto:deezstats@wetherall.pw">deezstats@wetherall.pw</a></p>
+		<p>Last Updated: This Terms Of Use was last updated on Saturday 20<sup>th</sup> June 2020.</p>	
+	</div>
